db0c92ae301eaaab20184b22972fe4356b52e323
shivam5676 <ssultanpura9@gmail.com>
2023-05-28 09:50:25 -0700

    async await return added inplace of .then block

--- src/App.js
+++ src/App.js
@@ -5,22 +5,37 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  function moviedetailsfetcher() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformeddata = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformeddata);
-      });
+  // function moviedetailsfetcher() {
+  //   fetch("https://swapi.dev/api/films/")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       const transformeddata = data.results.map((movieData) => {
+  //         return {
+  //           id: movieData.episode_id,
+  //           title: movieData.title,
+  //           openingText: movieData.opening_crawl,
+  //           releaseDate: movieData.release_date,
+  //         };
+  //       });
+  //       setMovies(transformeddata);
+  //     });
+  // }
+
+  async function moviedetailsfetcher() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+
+    const transformeddata = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformeddata);
   }
 
   return (
